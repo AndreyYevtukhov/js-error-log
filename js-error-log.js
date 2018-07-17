03990cf6abeff635706b8034cb3ea0bd87aa891d
AndreyYevtukhov <a.yevtukhov@gmail.com>
2018-07-17 09:49:28 +0300

    Change Promise unhandled rejection error's message formatting
    Additional:
    - #20856 TypeError: undefined is not an object (evaluating 'error.reason.message') was fixed
    - close bracket removed in clientInfo.CLN parameter

--- js-error-log.js
+++ js-error-log.js
@@ -1,174 +1,182 @@
 (function () {
-    let Logger = (function () {
-        /**
-         * stores POST endpoint url
-         */
-        let url;
+  let Logger = (function () {
+    /**
+     * stores POST endpoint url
+     */
+    let url;
 
-        /**
-         * can be used for log customisation
-         *
-         * this object will be merged with error, before message will be send
-         *
-         * @type {{}}
-         */
-        let defaults = {};
+    /**
+     * can be used for log customisation
+     *
+     * this object will be merged with error, before message will be send
+     *
+     * @type {{}}
+     */
+    let defaults = {};
 
-        /**
-         * initializes event listeners
-         */
-        function init() {
-            // log unhandled JS errors
-            window.onerror = function (type, location, line, position, errorObj) {
-                let message;
+    /**
+     * initializes event listeners
+     */
+    function init() {
+      // log unhandled JS errors
+      window.onerror = function (type, location, line, position, errorObj) {
+        let message;
 
-                if (type && location && line && position) {
-                    // standard error
-                    message = 'ERROR:' +
-                        ' \n: ' + type +
-                        ' \n\nat file: ' + location + ' ' +
-                        ' \nat line:' + line +
-                        ' \nat col: ' + position;
-                } else if (type !== null && typeof type === 'object') {
-                    // angular 2 app now sometimes wires EventErrors (???)
-                    // this code will try to get information from them
-                    message = 'Angular 2 EventError:' +
-                        ' \n' + ((type.message) ? type.message : ' -- ') +
-                        ' \n\nat file: ' + ((type.filename) ? type.filename : ' -- ') +
-                        ' \nat line: ' + ((type.lineno) ? type.lineno : ' -- ') +
-                        ' \nat col: ' + ((type.colno) ? type.colno : ' -- ') +
-                        ' \n\nstack: ' + ((type.error && type.error.stack) ? type.error.stack : ' -- ');
-                } else {
-                    // error data is invalid
-                    // log as much as we can
-                    message = 'UN-PROCESSABLE ERROR! All possible information:' +
-                    + type +
-                    ' \n\nlocation: ' + location +
-                    ' \nposition: ' + position +
-                    ' \n\nerrorObj: ' + (type !== null && typeof type === 'object') ? JSON.stringify(errorObj) : errorObj;
-                }
+        if (type && location && line && position) {
+          // standard error
+          message = 'ERROR:' +
+            ' \n: ' + type +
+            ' \n\nat file: ' + location + ' ' +
+            ' \nat line:' + line +
+            ' \nat col: ' + position;
+        } else if (type !== null && typeof type === 'object') {
+          // angular 2 app now sometimes wires EventErrors (???)
+          // this code will try to get information from them
+          message = 'Angular 2 EventError:' +
+            ' \n' + ((type.message) ? type.message : ' -- ') +
+            ' \n\nat file: ' + ((type.filename) ? type.filename : ' -- ') +
+            ' \nat line: ' + ((type.lineno) ? type.lineno : ' -- ') +
+            ' \nat col: ' + ((type.colno) ? type.colno : ' -- ') +
+            ' \n\nstack: ' + ((type.error && type.error.stack) ? type.error.stack : ' -- ');
+        } else {
+          // error data is invalid
+          // log as much as we can
+          message = 'UN-PROCESSABLE ERROR! All possible information:' +
+          +type +
+          ' \n\nlocation: ' + location +
+          ' \nposition: ' + position +
+          ' \n\nerrorObj: ' + (type !== null && typeof type === 'object') ? JSON.stringify(errorObj) : errorObj;
+        }
 
-                // fix for Safari issue
-                // https://bugs.webkit.org/show_bug.cgi?id=55092
-                if (errorObj !== undefined && errorObj.stack) {
-                    message += ' \n\nStack: ' + errorObj.stack;
-                }
+        // fix for Safari issue
+        // https://bugs.webkit.org/show_bug.cgi?id=55092
+        if (errorObj !== undefined && errorObj.stack) {
+          message += ' \n\nStack: ' + errorObj.stack;
+        }
 
-                sendMessage(message, 'error');
-            };
+        sendMessage(message, 'error');
+      };
 
-            // log errors inside promises
-            window.addEventListener("unhandledrejection", function (err) {
-                let message = 'Promise unhandled error' +
-                    ' \nmessage: ' + err.reason.message +
-                    ' \n\nstack: ' + err.reason.stack;
+      // log errors inside promises
+      window.addEventListener("unhandledrejection", function (err) {
+        let message;
 
-                sendMessage(message, 'error');
-            });
+        if (err && err.reason) {
+          message = 'Promise unhandled error' +
+            ' \nmessage: ' + err.reason.message +
+            ' \n\nstack: ' + err.reason.stack;
+        } else {
+          message = `Promise unhandled error without reason.
+                    
+                    ${JSON.stringify(err)}`;
+        }
 
-            // log jQuery AJAX errors
-            if (window.jQuery) {
-                $(document).ajaxError(function (event, xhr, options, exc) {
-                    let message = 'jQuery AJAX Error' +
-                        ' \n\nurl: ' + options.url +
-                        ' \nmethod: ' + options.type +
-                        ' \nstatus: ' + xhr.status +
-                        ' \nstatusText: ' + exc +
-                        ' \nresponse: ' + xhr.responseText;
+        sendMessage(message, 'error');
+      });
 
-                    message+= ` \n\nDebug info:
+      // log jQuery AJAX errors
+      if (window.jQuery) {
+        $(document).ajaxError(function (event, xhr, options, exc) {
+          let message = 'jQuery AJAX Error' +
+            ' \n\nurl: ' + options.url +
+            ' \nmethod: ' + options.type +
+            ' \nstatus: ' + xhr.status +
+            ' \nstatusText: ' + exc +
+            ' \nresponse: ' + xhr.responseText;
+
+          message += ` \n\nDebug info:
                         ${JSON.stringify(arguments)}`;
 
-                    sendMessage(message, 'error');
-                });
-            }
+          sendMessage(message, 'error');
+        });
+      }
+    }
+
+    /**
+     * sends POST request with formatted error message
+     *
+     * @param errorText: String
+     * @param errLevel: String
+     */
+    function sendMessage(errorText, errLevel) {
+      if (!url) {
+        throw new Error('[url] is not defined. You need to initialise logger first.');
+      }
+
+      let x = new XMLHttpRequest();
+
+      x.open('POST', url, true);
+      x.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+      let requestData = {};
+
+      let errorMessageWithPageDetails = errorText +
+        '\n\nClient info:' +
+        '\nURL: ' + window.location +
+        '\nCLN: ' + window.navigator.userAgent +
+        '\nX*Y: ' + window.innerWidth + 'x' + window.innerHeight;
+
+      if (defaults) {
+        requestData = Object.assign({}, defaults, {
+          errlevel: errLevel,
+          message: errorMessageWithPageDetails
+        });
+      } else {
+        requestData.message = errorMessageWithPageDetails;
+      }
+
+      x.send(JSON.stringify(requestData));
+    }
+
+    /**
+     * Logger API
+     */
+    return {
+      /**
+       * Initializes logger
+       *
+       * @param logPostURL String
+       * @param logDefaults Object
+       */
+      init: function (logPostURL, logDefaults) {
+        if (!logPostURL || typeof logPostURL !== 'string' || logPostURL instanceof String) {
+          throw new Error('[logPostURL] is required param. It must be a String.');
         }
 
-        /**
-         * sends POST request with formatted error message
-         *
-         * @param errorText: String
-         * @param errLevel: String
-         */
-        function sendMessage(errorText, errLevel) {
-            if (!url) {
-                throw new Error('[url] is not defined. You need to initialise logger first.');
-            }
+        url = logPostURL;
+        defaults = logDefaults;
 
-            let x = new XMLHttpRequest();
+        init();
+      },
 
-            x.open('POST', url, true);
-            x.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+      /**
+       * sends log message, if needed
+       */
+      sendMessage: sendMessage,
 
-            let requestData = {};
-
-            let errorMessageWithPageDetails = errorText +
-                '\n\nClient info:' +
-                '\nURL: ' + window.location +
-                '\nCLN: ' + window.navigator.userAgent + ']' +
-                '\nX*Y: ' + window.innerWidth + 'x' + window.innerHeight;
-
-            if (defaults) {
-                requestData = Object.assign({}, defaults, {
-                    errlevel: errLevel,
-                    message: errorMessageWithPageDetails
-                });
-            } else {
-                requestData.message = errorMessageWithPageDetails;
-            }
-
-            x.send(JSON.stringify(requestData));
-        }
-
-        /**
-         * Logger API
-         */
-        return {
-            /**
-             * Initializes logger
-             *
-             * @param logPostURL String
-             * @param logDefaults Object
-             */
-            init: function (logPostURL, logDefaults) {
-                if (!logPostURL || typeof logPostURL !== 'string' || logPostURL instanceof String) {
-                    throw new Error('[logPostURL] is required param. It must be a String.');
-                }
-
-                url = logPostURL;
-                defaults = logDefaults;
-
-                init();
-            },
-
-            /**
-             * sends log message, if needed
-             */
-            sendMessage: sendMessage,
-
-            /**
-             * returns logger initialisation state
-             * can be used for local debug, if you don't want to send log message some where,
-             * but want to log it in console
-             *
-             * @example
-             * if (Logger.isInit()) {
+      /**
+       * returns logger initialisation state
+       * can be used for local debug, if you don't want to send log message some where,
+       * but want to log it in console
+       *
+       * @example
+       * if (Logger.isInit()) {
              *  Logger.sendMessage(errorMessage);
              * } else {
              *  console.error(errorMessage);
              * }
-             *
-             * @returns {boolean}
-             */
-            isInit: function () {
-                return !!url;
-            }
-        }
-    })();
-
-    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = Logger;
-    } else {
-        window.Logger = Logger;
+       *
+       * @returns {boolean}
+       */
+      isInit: function () {
+        return !!url;
+      }
     }
+  })();
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = Logger;
+  } else {
+    window.Logger = Logger;
+  }
 })();
